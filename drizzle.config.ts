c43efe39cfbbd78d7e47217bb7f7399aaf740fe8
Hariom-Singh-001 <singhhariom9757@gmail.com>
2026-01-28 13:26:02 +0000

    upadating drizzle

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "drizzle-kit";
+import "dotenv/config"; // <--- This is the magic line that was missing!
 
 if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL, ensure the database is provisioned");
+  throw new Error("DATABASE_URL is missing");
 }
 
 export default defineConfig({
